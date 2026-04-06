fb6bd24a8aebebb4f035512e902a2445fbe5426b
Cauteros974 <pusik6666@ukr.net>
2026-04-06 20:28:54 +0300

    Update useUserStore.ts

--- store/useUserStore.ts
+++ store/useUserStore.ts
@@ -1,4 +1,4 @@
-
+import {create} from 'zustand';
 interface UserState{
     fullName: string;
     weight: number;
@@ -6,4 +6,9 @@ interface UserState{
     dailyCalories: number;
     dailyWeight: number;   
     dailyWater: number;
+    etProfile: (data: { fullName: string; weight: number; height: number }) => void;
+}
+
+export const useUserStore = create () => {
+
 }
