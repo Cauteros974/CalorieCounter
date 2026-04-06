6cc67a2e66d6c960dc19ee47967cf557a85a09ff
Cauteros974 <pusik6666@ukr.net>
2026-04-06 21:09:21 +0300

    commit

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -4,4 +4,4 @@ const profileSchema = z.object ({
     fullName: z.string().min(1, 'The name is too short'),
     weight: z.string().transform(Number).pipe(z.number().min(30).max(300)),
     height: z.string().transform(Number).pipe(z.number().min(100).max(250)),
-})
+});
