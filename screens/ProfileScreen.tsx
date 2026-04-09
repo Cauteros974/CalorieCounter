27da2034b08d6b244e492a0f51e62e68b285896f
Cauteros974 <pusik6666@ukr.net>
2026-04-09 20:18:45 +0300

    Update ProfileScreen.tsx

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -8,8 +8,10 @@ import { useUserStore } from '../store/useUserStore';
 //Validation Scheme
 const profileSchema = z.object ({
     fullName: z.string().min(1, 'The name is too short'),
-    weight: z.string().transform(Number).pipe(z.number().min(30).max(300)),
-    height: z.string().transform(Number).pipe(z.number().min(100).max(250)),
+    weight: z.string().refine(val => {
+        const num = Number(val);
+        return num >= 30 && num <= 300;
+    }, 'invalid weight'),
 });
 
 type ProfileFormData = z.infer<typeof profileSchema>;
