a88a6eedff74103851f9353945a97b54a7c5f602
Cauteros974 <pusik6666@ukr.net>
2026-04-09 20:11:18 +0300

    Update ProfileScreen.tsx

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -85,5 +85,10 @@ export default function profileScreen() {
                 <Text style={styles.buttonText}>Calculate the standards</Text>
             </TouchableOpacity>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 20, backgroundColor: '#fff', justifyContent: 'center' },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 30, textAlign: 'center' },}
+});
