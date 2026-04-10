f5cbf2dfd010dc8deb0a12526f393c987ac99793
Cauteros974 <pusik6666@ukr.net>
2026-04-10 21:32:49 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
+import HomeScreen from '@/screens/HomeScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Home } from 'lucide-react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,8 +10,16 @@ export default  function App() {
     <NavigationContainer>
       <Tab.Navigator screenOptions={{
         headerShown: false,
+        tabBarStyle: { height: 90, paddingTop: 10 },
+        tabBarActiveTintColor: '#4CAF50'
       }}>
-
+        <Tab.Screen 
+          name='Home'
+          component={HomeScreen}
+          options={{ tabBarIcon: ({ color }) => <Home color={color} size={24} /> }}
+        >
+          
+        </Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
   )
