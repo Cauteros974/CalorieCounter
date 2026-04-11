9661627bde3530bee89dba7f4975d9ca3409ad32
Cauteros974 <pusik6666@ukr.net>
2026-04-11 21:16:11 +0300

    Update notificationService.ts

--- services/notificationService.ts
+++ services/notificationService.ts
@@ -4,3 +4,13 @@ export async function requestPermissions() {
     const {status} = await Notifications.requestPermissionsAsync();
     return status === 'granted';
 }
+
+export async function scheduleWaterRemindername() {
+    await Notifications.cancelAllScheduledNotificationsAsync();
+
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title: "Time to drink wate"
+        }
+    })
+}
