e7dc1a11663d628c80b6ccade747fed19ec6b044
Cauteros974 <pusik6666@ukr.net>
2026-04-11 17:50:37 +0300

    Update CameraScreen.tsx

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,7 +1,20 @@
 import { useCameraPermissions } from 'expo-camera';
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+import { View } from 'react-native';
+import { useUserStore } from "../store/useUserStore";
 
 export default function CameraScreen({onClose}: {onClose: () => void}) {
     const [permision, requestPermission] = useCameraPermissions();
     const [loading, setLoading] = useState(false);
+    const cameraRef = useRef<any>(null);
+    const addCalories = useUserStore(state => state.addCalories);
+
+    if(!permision) return <View />
+
+    const takePhoto = async () => {
+        if(cameraRef.current) {
+            setLoading(true);
+            const photo = await cameraRef.current.takePictureAsync()
+        }
+    }
 }
