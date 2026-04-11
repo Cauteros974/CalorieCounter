8f8e6bf7c9f02e34114edc3dd6ce42391e7ce7ea
Cauteros974 <pusik6666@ukr.net>
2026-04-11 18:18:30 +0300

    Update CameraScreen.tsx

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -65,4 +65,5 @@ const styles = StyleSheet.create({
     overlay: { flex: 1, justifyContent: 'space-between', alignItems: 'center', paddingVertical: 50 },
     closeBtn: { alignSelf: 'flex-start', marginLeft: 20 },
     captureBtn: { width: 80, height: 80, borderRadius: 40, borderWidth: 4, borderColor: '#fff', justifyContent: 'center', alignItems: 'center' },
+    captureInner: { width: 60, height: 60, borderRadius: 30, backgroundColor: '#fff' }
 })
