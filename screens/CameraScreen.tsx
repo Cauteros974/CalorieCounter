ae10d2210130d401f49917661a8585314774084c
Cauteros974 <pusik6666@ukr.net>
2026-04-11 17:54:28 +0300

    Update CameraScreen.tsx

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,16 +1,24 @@
 import { useCameraPermissions } from 'expo-camera';
-import React, { useRef, useState } from "react";
-import { View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { analyzeFoodImage } from '../services/aiService';
-import { useUserStore } from "../store/useUserStore";
+import { useUserStore } from '../store/useUserStore';
 
 export default function CameraScreen({onClose}: {onClose: () => void}) {
-    const [permision, requestPermission] = useCameraPermissions();
+    const [permission, requestPermission] = useCameraPermissions();
     const [loading, setLoading] = useState(false);
     const cameraRef = useRef<any>(null);
     const addCalories = useUserStore(state => state.addCalories);
 
-    if(!permision) return <View />
+    if(!permission) return <View />
+    if (!permission.granted) {
+        return (
+          <View style={styles.container}>
+            <Text>Need access to camera</Text>
+            <TouchableOpacity onPress={requestPermission}><Text>Allow</Text></TouchableOpacity>
+          </View>
+        );
+      }
 
     const takePhoto = async () => {
         if(cameraRef.current) {
