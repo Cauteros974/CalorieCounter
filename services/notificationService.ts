008455fd523a1e2ff21916a96a579afe88409966
Cauteros974 <pusik6666@ukr.net>
2026-04-11 21:22:17 +0300

    Update notificationService.ts

--- services/notificationService.ts
+++ services/notificationService.ts
@@ -1,18 +1,10 @@
 import * as Notifications from 'expo-notifications';
 
-export async function requestPermissions() {
-    const {status} = await Notifications.requestPermissionsAsync();
-    return status === 'granted';
-}
-
-export async function scheduleWaterRemindername() {
-    await Notifications.cancelAllScheduledNotificationsAsync();
-
-    await Notifications.scheduleNotificationAsync({
-        content: {
-            title: "It's time to drink some water! 💧",
-            body: "Maintain hydration for optimal well-being",
-        },
-        trigger: {seconds: 7200, repeats: true},
-    });
-}
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowBanner: true,
+      shouldShowList: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
