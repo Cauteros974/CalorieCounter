b6664044011aa0f17a50c7de24aa7f4e0ddd68c0
Cauteros974 <pusik6666@ukr.net>
2026-04-12 18:05:30 +0300

    Update StatisticsScreen.tsx

---
+++ screens/StatisticsScreen.tsx
@@ -0,0 +1,17 @@
+import { Colors, useUserStore } from '../store/useUserStore';
+
+export default function StatisticsScreen() {
+    const {theme} = useUserStore();
+    const currentColors = Colors[theme];
+
+    const data = {
+        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "San"],
+        datasets: [
+            {
+                data: [1800, 2100, 1950, 2400, 1700, 2000, 2200],
+                color: (opacity = 1) => currentColors.primary, 
+                strokeWidth: 3
+            }
+        ]
+    }
+}
