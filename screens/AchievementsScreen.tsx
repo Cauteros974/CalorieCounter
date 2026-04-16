48c10475348ff77a6a34e9b68a0c7e7dd379cca8
Cauteros974 <pusik6666@ukr.net>
2026-04-16 19:40:59 +0300

    Update AchievementsScreen.tsx

--- screens/AchievementsScreen.tsx
+++ screens/AchievementsScreen.tsx
@@ -1,6 +1,18 @@
 import { Colors, useUserStore } from "@/store/useUserStore";
+import React from "react";
+import { StyleSheet, View } from 'react-native';
 
 export default function AchievementsScreen() {
     const {achievements, level, xp, streak, theme} = useUserStore();
     const currentColors = Colors[theme];
+
+    return(
+        <View style={[styles.container, {backgroundColor: currentColors.background}]}>
+            
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {padding: 10}
+})
