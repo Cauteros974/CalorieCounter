5164126de8fbf2ecd82fd33ce29b8e4ff69a8c7f
Cauteros974 <pusik6666@ukr.net>
2026-04-18 17:51:44 +0300

    Update AchievementPopu.tsx

--- components/AchievementPopu.tsx
+++ components/AchievementPopu.tsx
@@ -66,5 +66,9 @@ const styles = StyleSheet.create({
         padding: 30,
         alignItems: 'center',
         elevation: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.3,
+        shadowRadius: 20
     }
 })
