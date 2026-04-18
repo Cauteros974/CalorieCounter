47f17484c48bf9d8470b164886caf52cc31c5c35
Cauteros974 <pusik6666@ukr.net>
2026-04-18 17:39:23 +0300

    Update AchievementPopu.tsx

--- components/AchievementPopu.tsx
+++ components/AchievementPopu.tsx
@@ -1,6 +1,6 @@
 import { Colors, useUserStore } from '@/store/useUserStore';
-import { useRef } from 'react';
-import { Dimensions } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Animated, Dimensions } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
@@ -8,4 +8,15 @@ export default function AchievementPopup() {
     const { newlyUnlockedAchievement, closeCongratulation, theme } = useUserStore();
     const currentColors = Colors[theme];
     const confettiRef = useRef<any>(null);
+
+    const scale = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        if (newlyUnlockedAchievement){
+            Animated.spring(scale, {
+                toValue: 1,
+                useNativeDriver: true,
+            }).start();
+        }
+    })
 }
