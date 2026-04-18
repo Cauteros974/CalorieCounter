ff133baabc257404d61adf5c170fb1ff51a58e03
Cauteros974 <pusik6666@ukr.net>
2026-04-18 18:28:04 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,50 +1,79 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native'; // Добавляем проверку платформы
+
+import AchievementPopup from '@/components/AchievementPopu';
 import AchievementsScreen from '@/screens/AchievementsScreen';
 import HomeScreen from '@/screens/HomeScreen';
 import ProfileScreen from '@/screens/ProfileScreen';
 import StatisticsScreen from '@/screens/StatisticsScreen';
+
 import { requestPermissions } from '@/services/notificationService';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Colors, useUserStore } from '@/store/useUserStore';
 import { ChartColumn, Home, Trophy, User } from 'lucide-react-native';
-import { useEffect } from 'react';
 
 const Tab = createBottomTabNavigator();
 
-export default  function App() {
+export default function App() {
+  const { theme } = useUserStore();
+  const currentColors = Colors[theme];
 
   useEffect(() => {
-    requestPermissions();
+    if (Platform.OS !== 'web') {
+      requestPermissions();
+    }
   }, []);
 
+  return (
+    <>
+      <AchievementPopup />
+      <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
 
-  return(
       <Tab.Navigator screenOptions={{
         headerShown: false,
-        tabBarStyle: { height: 90, paddingTop: 10 },
-        tabBarActiveTintColor: '#4CAF50'
+        tabBarStyle: { 
+          height: 90, 
+          paddingTop: 10,
+          backgroundColor: currentColors.card,
+          borderTopColor: currentColors.border 
+        },
+        tabBarActiveTintColor: currentColors.primary,
+        tabBarInactiveTintColor: currentColors.subText,
       }}>
         <Tab.Screen 
           name='Home'
           component={HomeScreen}
-          options={{ tabBarIcon: ({ color }) => <Home color={color} size={24} /> }}
+          options={{ 
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => <Home color={color} size={24} /> 
+          }}
         />
-        
-        <Tab.Screen 
-          name='Profile'
-          component={ProfileScreen}
-          options={{ tabBarIcon: ({ color }) => <User color={color} size={24} /> }}
-        />
-
         <Tab.Screen
           name='Graph'
           component={StatisticsScreen}
-          options={{ tabBarIcon: ({ color }) => <ChartColumn color={color} size={24} /> }}
+          options={{ 
+            tabBarLabel: 'Statistics',
+            tabBarIcon: ({ color }) => <ChartColumn color={color} size={24} /> 
+          }}
         />
-
         <Tab.Screen
           name='Achive'
           component={AchievementsScreen}
-          options={{ tabBarIcon: ({ color }) => <Trophy color={color} size={24} /> }}
+          options={{ 
+            tabBarLabel: 'Awards',
+            tabBarIcon: ({ color }) => <Trophy color={color} size={24} /> 
+          }}
+        />
+        <Tab.Screen 
+          name='Profile'
+          component={ProfileScreen}
+          options={{ 
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({ color }) => <User color={color} size={24} /> 
+          }}
         />
       </Tab.Navigator>
-  )
+    </>
+  );
 }
