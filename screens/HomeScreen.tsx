0a13d76e462de3cb607dd97a0fda8d47947ad198
Cauteros974 <pusik6666@ukr.net>
2026-04-24 20:49:16 +0300

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import {
     Animated
 } from 'react-native';
@@ -6,5 +7,7 @@ import { Circle } from 'react-native-svg';
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 function CalorieRing({progress}: {progress: number}){
-    
+    const animatedValue = useRef(new Animated.Value(0)).current;
+    const SIZE = 160;
+    const STROKE = 12;
 }
