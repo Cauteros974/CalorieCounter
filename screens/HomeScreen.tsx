3e2eb48607d379c18db334ea4f109e41bd819f3f
Cauteros974 <pusik6666@ukr.net>
2026-04-25 12:47:09 +0300

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -200,6 +200,13 @@ export default function HomeScreen() {
                     <Text style={{ fontSize: 12, fontWeight: '700', color: currentColors.subText }}>{label}</Text>
                     <Text style={{ fontSize: 12, color: currentColors.text }}>{current}g</Text>
                 </View>
+                <View style={{height: 6, backgroundColor: '#F0F0F0', borderRadius: 3, overflow: 'hidden'}}>
+                    <Animated.View 
+                        style={{
+                            height: '100%'
+                        }}
+                    />
+                </View>
              </View>
         )
     }
