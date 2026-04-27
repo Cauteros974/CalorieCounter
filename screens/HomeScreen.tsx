21fad0ac23ede9b595a60d60fa1ad09605ce467d
Cauteros974 <pusik6666@ukr.net>
2026-04-27 20:23:39 +0300

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -289,6 +289,11 @@ export default function HomeScreen() {
                                     <Text style={styles.ringStatValue}>{consumedCalories}</Text>
                                     <Text style={styles.ringStatLabel}>eaten</Text>
                                 </View>
+                                <View style={[styles.ringStatDivider]}/>
+                                <View style={styles.ringStatItem}>
+                                    <Text style={styles.ringStatValue}>{dailyCalories}</Text>
+                                    <Text style={styles.ringStatLabel}>goal</Text>
+                                </View>
                             </View>
                         </View>
                     </View>
@@ -336,5 +341,6 @@ const styles = StyleSheet.create({
     ringStats: {flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', },
     ringStatItem: { alignItems: 'center' },
     ringStatValue: { fontSize: 20, fontWeight: '800', color: '#1A1A1A' },
-    ringStatLabel: {fontSize: 12, color: '#888', marginTop: 2}
+    ringStatLabel: {fontSize: 12, color: '#888', marginTop: 2},
+    ringStatDivider: {width: 1, height: 40, backgroundColor: '#EFEFEF'}
 })
