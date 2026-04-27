3b45f38f2d9dc985194b36512aba85a1e396c7e5
Cauteros974 <pusik6666@ukr.net>
2026-04-27 21:00:20 +0300

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -406,5 +406,5 @@ const styles = StyleSheet.create({
     mealsHeaderRow: {flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 10, marginBottom: 15},
     sectionTitle: { fontSize: 18, fontWeight: '700' },
     totalStats: { fontSize: 14, color: '#888', fontWeight: '500' },
-    mealCard: {backgroundColor: '#fff', borderRadius: 18, padding: 18}
+    mealCard: {backgroundColor: '#fff', borderRadius: 18, padding: 18, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 12, borderLeftWidth: 5, elevation: 2}
 })
