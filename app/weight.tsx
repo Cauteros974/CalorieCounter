52596a2cfb051150456ac418ba68951c1489ef0e
Cauteros974 <pusik6666@ukr.net>
2026-05-06 20:24:12 +0300

    Update weight.tsx

--- app/weight.tsx
+++ app/weight.tsx
@@ -1,7 +1,20 @@
+import React from 'react';
 import {
-    Dimensions
+    Dimensions,
+    Text,
+    View
 } from 'react-native';
 
 const W = Dimensions.get('window').width - 80;
 const H = 160;
 const PADDING_LEFT = 40;
+
+function WeightChart({data} : {data: {data: string; value: number}[]}) {
+    if(data.length < 1) {
+        return(
+            <View style={{ alignItems: 'center' }}>
+                <Text style={{ color: '#AAA', fontSize: 14 }}>Add at least 2 entries to see the chart</Text>
+            </View>
+        )
+    }
+}
