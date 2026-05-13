0687faee57c69c15a31319a7ef790d33fb8bc345
Cauteros974 <pusik6666@ukr.net>
2026-05-13 20:17:25 +0300

    Update album.tsx

--- app/album.tsx
+++ app/album.tsx
@@ -1,5 +1,21 @@
-import { Dimensions } from "react-native";
+import { useUserStore } from "@/store/useUserStore";
+import { ArrowLeft } from "lucide-react-native";
+import { Dimensions, TouchableOpacity, View } from "react-native";
 
 const COL = 2;
 const GAP = 12;
 const CARD_W = (Dimensions.get('window').width - 40 - GAP) / COL;
+
+export default function AlbumScreen() {
+    const {photo} = useUserStore;
+
+    return(
+        <View style = {styles.container}>
+            <View style = {styles.header}>
+                <TouchableOpacity>
+                    <ArrowLeft color="#1A1A1A" size={20} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
